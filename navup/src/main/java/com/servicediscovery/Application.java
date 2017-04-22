cdca176af094c7872a8b1e1b89bf908eff51b7d6
Siphokazi Manana <siphokazi.manana@advance.io>
2017-04-22 20:47:23 +0200

    integration server

---
+++ navup/src/main/java/com/servicediscovery/Application.java
@@ -0,0 +1,16 @@
+package com.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableZuulProxy
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
